a9f33a56271d80f9ff688bb877b63aa216c33aa8
Pavel Svec <pavel.svec@itcrawl.cz>
2019-08-22 13:45:08 +0200

    First is bigger

--- GenericClassAndFunc/Program.cs
+++ GenericClassAndFunc/Program.cs
@@ -12,7 +12,7 @@ namespace GenericClassAndFunc
             b = x;
         }
 
-        public bool IsBigger<T>(T a, T b) where T : IComparable
+        public static bool FirstIsBigger<T>(T a, T b) where T : IComparable
         {
             return a.CompareTo(b) > 0;
         }
@@ -22,11 +22,16 @@ namespace GenericClassAndFunc
             int ix = 0, iy = 4;
             double dx = 2.1, dy = 3.5;
 
+            Console.WriteLine($"{ix}, {iy}, první je větší: {FirstIsBigger(ix, iy)}");
+            Console.WriteLine($"{dx}, {dy}, první je větší: {FirstIsBigger(dx, dy)}");
+
             Swap(ref ix, ref iy);
             Swap(ref dx, ref dy);
 
-            Console.WriteLine($"{ix}, {iy}");
-            Console.WriteLine($"{dx}, {dy}");
+            Console.WriteLine("\nPo volání Swap:");
+
+            Console.WriteLine($"{ix}, {iy}, první je větší: {FirstIsBigger(ix, iy)}");
+            Console.WriteLine($"{dx}, {dy}, první je větší: {FirstIsBigger(dx, dy)}");
 
             Console.ReadLine();
         }
