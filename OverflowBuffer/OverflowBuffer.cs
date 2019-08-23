01232e7463bcb26ec1a6b47a9042815929065855
Pavel Svec <pavel.svec@itcrawl.cz>
2019-08-23 14:16:19 +0200

    OverflowBuffer generic class

---
+++ OverflowBuffer/OverflowBuffer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverflowBufferNS
+{
+    class OverflowBuffer<T>
+    {
+        T[] buff;
+        int position;
+        int count;
+
+        public OverflowBuffer(int size = 10)
+        {
+            buff = new T[size];
+            position = -1;
+            count = 0;
+        }
+
+        public void Add(T x)
+        {
+            buff[++position] = x;
+            count++;
+            if (position == buff.Length - 1)
+                position = -1;
+        }
+
+        public void Add(T[] xArr)
+        {
+            foreach (var x in xArr)
+                Add(x);
+        }
+
+        public void List()
+        {
+            Console.WriteLine(buff[position]);
+
+            int i = (position != 0) ? position-1 : buff.Length-1;
+            while ( i != (count%buff.Length)-1 )
+            {
+                Console.WriteLine(buff[i]);
+                if (i == 0)
+                    i = buff.Length;
+                i--;
+            }
+        }
+    }
+}
---
+++ OverflowBuffer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OverflowBufferNS;
+
+namespace OverflowBufferNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OverflowBuffer<int> ov = new OverflowBuffer<int>();
+
+            int[] iArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+            ov.Add(iArr);
+            ov.List();
+        }
+    }
+}
